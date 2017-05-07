a95fe0540bb7e79babb90d8493c65fc37fbb9002
surbhi singh <surbhi.singh0001@gmail.com>
2017-05-07 23:47:35 +1000

    final code update

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # frontend-candidate-test
 Front End test
+
+# hCard Builder
+It is an application built in plain Html, CSS and plain Javascript. If you can work in plain Javascript, you can work in any language.
+
+### Functionality
+
+* As the form is filled out, the preview is updated automatically side by side.
+* When the user selects an image, the thumbnail is updated.
+* The app is responsive and follows a mobile first approach
+* The form should work in all the major browsers
+
+
+### Run the application
+As there is no server involved, you can directly run the application.
+
+### time taken
+Time taken for this assignment is 4 hours approximately
--- js/custom.js
+++ js/custom.js
@@ -13,22 +13,19 @@
     for (var i = 0; i < inputs.length; i++) {
         inputs[i].addEventListener('change', updatePreview, false);
     }
-   
-
-   document.getElementById('upload-button').addEventListener('change', readURL, true);
-function readURL(){
+   function readURL(){
     var file = document.getElementById("upload-button").files[0];
 			var reader = new FileReader();
 			reader.onloadend = function(){
 				document.getElementById('preview-img').style.backgroundImage = "url(" + reader.result + ")";        
-			}
+			};
 			if(file){
 				reader.readAsDataURL(file);
-			}else{
 			}
 }
 
+   document.getElementById('upload-button').addEventListener('change', readURL, true);
+
+
       
 }());
-
-
